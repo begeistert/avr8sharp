aaf55f5a8bf44ef0d398feb72d3f4540ab57f64f
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2025-01-12 00:17:05 -0600

    Implement Watchdog

---
+++ AVR8Sharp/Peripherals/Watchdog.cs
@@ -0,0 +1,141 @@
+using AVR8Sharp.Cpu;
+namespace AVR8Sharp.Peripherals;
+
+public class Watchdog
+{
+	// Register Bits
+	const int MCUSR_WDRF = 0x10; // Watchdog System Reset Flag
+	
+	const int WDTCSR_WDIF = 0x80; // Watchdog Interrupt Flag
+	const int WDTCSR_WDIE = 0x40; // Watchdog Interrupt Enable
+	const int WDTCSR_WDP3 = 0x20; // Watchdog Timer Prescaler
+	const int WDTCSR_WDCE = 0x10; // Watchdog Change Enable
+	const int WDTCSR_WDE = 0x08; // Watchdog System Reset Enable
+	const int WDTCSR_WDP2 = 0x04; // Watchdog Timer Prescaler
+	const int WDTCSR_WDP1 = 0x02; // Watchdog Timer Prescaler
+	const int WDTCSR_WDP0 = 0x01; // Watchdog Timer Prescaler
+	const int WDTCSR_WDP210 = WDTCSR_WDP2 | WDTCSR_WDP1 | WDTCSR_WDP0;
+	
+	const int WDTCSR_PROTECT_MASK = WDTCSR_WDE | WDTCSR_WDP3 | WDTCSR_WDP210;
+	
+	public static WatchdogConfig WatchdogConfig = new WatchdogConfig {
+		WatchdogInterrupt = 0x0c,
+		
+		MCUSR = 0x54,
+		WDTCSR = 0x60
+	};
+	
+	readonly long _clockFrequency = 128_000;
+	
+	private Cpu.Cpu _cpu;
+	private WatchdogConfig _config;
+	private AvrClock _clock;
+	
+	private int _changeEnabledCycles = 0;
+	private int _watchdogTimeout = 0;
+	private bool _enabledValue = false;
+	private bool _scheduled = false;
+	
+	private AvrInterruptConfig _watchdog;
+	
+	public bool Enabled {
+		get {
+			return _enabledValue;
+		}
+	}
+	
+	/// <summary>
+	/// The base clock frequency is 128KHz. Thus, a prescaler of 2048 gives 16ms timeout.
+	/// </summary>
+	public double Prescaler {
+		get {
+			var wdtcsr = _cpu.Data[_config.WDTCSR];
+			var value = ((wdtcsr & WDTCSR_WDP3) >> 2) | (wdtcsr & WDTCSR_WDP210);
+			return 2048 << value;
+		}
+	}
+	
+	public Watchdog (Cpu.Cpu cpu, WatchdogConfig config, AvrClock clock)
+	{
+		_cpu = cpu;
+		_clock = clock;
+		
+		_watchdog = new AvrInterruptConfig {
+			Address = config.WatchdogInterrupt,
+			FlagRegister = config.WDTCSR,
+			FlagMask = WDTCSR_WDIF,
+			EnableRegister = config.WDTCSR,
+			EnableMask = WDTCSR_WDIE
+		};
+
+		_cpu.OnWatchdogReset = () => {
+			ResetWatchdog ();
+		};
+		
+		_cpu.WriteHooks[config.WDTCSR] = (value, oldValue, _, _) => {
+			if ((value & WDTCSR_WDCE) != 0 && (value & WDTCSR_WDE) != 0) {
+				_changeEnabledCycles = _cpu.Cycles + 4;
+				value = (byte)(value & ~WDTCSR_PROTECT_MASK);
+			} else {
+				if (_cpu.Cycles >= _changeEnabledCycles) {
+					value = (byte)((value & ~WDTCSR_PROTECT_MASK) | (oldValue & WDTCSR_PROTECT_MASK));
+				}
+				_enabledValue = (value & WDTCSR_WDE) != 0 || (value & WDTCSR_WDIE) != 0;
+				_cpu.Data[config.WDTCSR] = value;
+			}
+			
+			if (Enabled)
+				ResetWatchdog ();
+			
+			if (Enabled && !_scheduled) {
+				_cpu.AddClockEvent (CheckWatchdog, _watchdogTimeout - _cpu.Cycles);
+			}
+			
+			_cpu.ClearInterruptByFlag (_watchdog, value);
+			return true;
+		};
+	}
+	
+	private void ResetWatchdog ()
+	{
+		var cycles = (int)Math.Floor ((_clock.Frequency / _clockFrequency) * Prescaler);
+		_watchdogTimeout = _cpu.Cycles + cycles;
+	}
+	
+	private void CheckWatchdog ()
+	{
+		if (Enabled && _cpu.Cycles >= _watchdogTimeout) {
+			// Watchdog timed out!
+			var wdtcsr = _cpu.Data[_config.WDTCSR];
+			if ((wdtcsr & WDTCSR_WDIE) != 0) {
+				_cpu.SetInterruptFlag (_watchdog);
+			}
+			if ((wdtcsr & WDTCSR_WDE) != 0) {
+				if ((wdtcsr & WDTCSR_WDIE) != 0) {
+					_cpu.Data[_config.WDTCSR] &= ~WDTCSR_WDIE & 0xff;
+				} else {
+					_cpu.Reset ();
+					_scheduled = false;
+					_cpu.Data[_config.MCUSR] |= MCUSR_WDRF;
+					return;
+				}
+			}
+			ResetWatchdog ();
+		}
+		
+		if (Enabled) {
+			_scheduled = true;
+			_cpu.AddClockEvent (CheckWatchdog, _watchdogTimeout - _cpu.Cycles);
+		} else {
+			_scheduled = false;
+		}
+	}
+}
+
+public struct WatchdogConfig
+{
+	public byte WatchdogInterrupt;
+	
+	public byte MCUSR;
+	public byte WDTCSR;
+}

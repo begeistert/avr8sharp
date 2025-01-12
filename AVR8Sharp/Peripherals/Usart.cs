ba635658ef23af030aa72085bcf23b1d0923bac2
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2025-01-11 23:38:15 -0600

    Implement USART

---
+++ AVR8Sharp/Peripherals/Usart.cs
@@ -0,0 +1,288 @@
+using System.Text;
+using AVR8Sharp.Cpu;
+namespace AVR8Sharp.Peripherals;
+
+public class AvrUsart
+{
+	const int UCSRA_RXC = 0x80; // USART Receive Complete
+	const int UCSRA_TXC = 0x40; // USART Transmit Complete
+	const int UCSRA_UDRE = 0x20; // USART Data Register Empty
+	const int UCSRA_FE = 0x10; // Frame Error
+	const int UCSRA_DOR = 0x08; // Data OverRun
+	const int UCSRA_UPE = 0x04; // USART Parity Error
+	const int UCSRA_U2X = 0x02; // Double the USART Transmission Speed
+	const int UCSRA_MPCM = 0x01; // Multi-processor Communication Mode
+	const int UCSRA_CFG_MASK = UCSRA_U2X;
+	const int UCSRB_RXCIE = 0x80; // RX Complete Interrupt Enable
+	const int UCSRB_TXCIE = 0x40; // TX Complete Interrupt Enable
+	const int UCSRB_UDRIE = 0x20; // USART Data Register Empty Interrupt Enable
+	const int UCSRB_RXEN = 0x10; // Receiver Enable
+	const int UCSRB_TXEN = 0x08; // Transmitter Enable
+	const int UCSRB_UCSZ2 = 0x04; // Character Size
+	const int UCSRB_RXB8 = 0x02; // Receive Data Bit 8
+	const int UCSRB_TXB8 = 0x01; // Transmit Data Bit 8
+	const int UCSRB_CFG_MASK = UCSRB_UCSZ2 | UCSRB_RXEN | UCSRB_TXEN;
+	const int UCSRC_UMSEL1 = 0x80; // USART Mode Select 1
+	const int UCSRC_UMSEL0 = 0x40; // USART Mode Select 0
+	const int UCSRC_UPM1 = 0x20; // Parity Mode 1
+	const int UCSRC_UPM0 = 0x10; // Parity Mode 0
+	const int UCSRC_USBS = 0x08; // Stop Bit Select
+	const int UCSRC_UCSZ1 = 0x04; // Character Size
+	const int UCSRC_UCSZ0 = 0x02; // Character Size
+	const int UCSRC_UCPOL = 0x01; // Clock Polarity
+	
+	
+	public static UsartConfig Usart0Config = new UsartConfig {
+		RxCompleteInterrupt = 0x24,
+		DataRegisterEmptyInterrupt = 0x26,
+		TxCompleteInterrupt = 0x28,
+		UCSRA = 0xc0,
+		UCSRB = 0xc1,
+		UCSRC = 0xc2,
+		UBRRL = 0xc4,
+		UBRRH = 0xc5,
+		UDR = 0xc6,
+	};
+	public static Dictionary<int, int> RxMasks = new Dictionary<int, int> {
+		{ 5, 0x1f },
+		{ 6, 0x3f },
+		{ 7, 0x7f },
+		{ 8, 0xff },
+		{ 9, 0xff },
+	};
+	
+	private Cpu.Cpu _cpu;
+	private UsartConfig _config;
+	private uint _freqHz;
+	
+	private bool _rxBusyValue = false;
+	private byte _rxByte = 0;
+	private StringBuilder _lineBuffer = new StringBuilder();
+	
+	private AvrInterruptConfig _rxc;
+	private AvrInterruptConfig _udre;
+	private AvrInterruptConfig _txc;
+	
+	public Action<byte>? OnByteTransmit { get; set; } = null;
+	public Action<string>? OnLineTransmit { get; set; } = null;
+	public Action? OnRxComplete { get; set; } = null;
+	public Action? OnConfigurationChange { get; set; } = null;
+	
+	public bool RxBusy {
+		get {
+			return _rxBusyValue;
+		}
+	}
+	public int CyclesPerChar {
+		get {
+			var symbolsPerChar = 1 + BitsPerChar + StopBits + (ParityEnabled ? 1 : 0);
+			return (UBRR + 1) * Multiplier * symbolsPerChar;
+		}
+	}
+	public int UBRR {
+		get {
+			return _cpu.Data[_config.UBRRL] | _cpu.Data[_config.UBRRH] << 8;
+		}
+	}
+	public int Multiplier {
+		get {
+			return (_cpu.Data[_config.UCSRA] & UCSRA_U2X) != 0 ? 8 : 16;
+		}
+	}
+	public bool RxEnable {
+		get {
+			return (_cpu.Data[_config.UCSRB] & UCSRB_RXEN) != 0;
+		}
+	}
+	public bool TxEnable {
+		get {
+			return (_cpu.Data[_config.UCSRB] & UCSRB_TXEN) != 0;
+		}
+	}
+	public long BaudRate {
+		get {
+			return _freqHz / (Multiplier * (1 + UBRR));
+		}
+	}
+	public int BitsPerChar {
+		get {
+			var ucsz = ((_cpu.Data[_config.UCSRC] & (UCSRC_UCSZ1 | UCSRC_UCSZ0)) >> 1) | (_cpu.Data[_config.UCSRB] & UCSRB_UCSZ2);
+			switch (ucsz) {
+				case 0:
+					return 5;
+				case 1:
+					return 6;
+				case 2:
+					return 7;
+				case 3:
+					return 8;
+				default: // 4..6 are reserved
+				case 7:
+					return 9;
+			}
+		}
+	}
+	public int StopBits {
+		get {
+			return (_cpu.Data[_config.UCSRC] & UCSRC_USBS) != 0 ? 2 : 1;
+		}
+	}
+	public bool ParityEnabled {
+		get {
+			return (_cpu.Data[_config.UCSRC] & UCSRC_UPM1) != 0;
+		}
+	}
+	public bool ParityOdd {
+		get {
+			return (_cpu.Data[_config.UCSRC] & UCSRC_UPM0) != 0;
+		}
+	}
+
+	public AvrUsart (Cpu.Cpu cpu, UsartConfig config, uint freqHz)
+	{
+		_cpu = cpu;
+		_config = config;
+		_freqHz = freqHz;
+		
+		_rxc = new AvrInterruptConfig {
+			Address = _config.RxCompleteInterrupt,
+			FlagRegister = _config.UCSRA,
+			FlagMask = UCSRA_RXC,
+			EnableRegister = _config.UCSRB,
+			EnableMask = UCSRB_RXCIE,
+			Constant = true
+		};
+		_udre = new AvrInterruptConfig {
+			Address = _config.DataRegisterEmptyInterrupt,
+			FlagRegister = _config.UCSRA,
+			FlagMask = UCSRA_UDRE,
+			EnableRegister = _config.UCSRB,
+			EnableMask = UCSRB_UDRIE
+		};
+		_txc = new AvrInterruptConfig {
+			Address = _config.TxCompleteInterrupt,
+			FlagRegister = _config.UCSRA,
+			FlagMask = UCSRA_TXC,
+			EnableRegister = _config.UCSRB,
+			EnableMask = UCSRB_TXCIE
+		};
+		
+		Reset ();
+		
+		_cpu.WriteHooks[_config.UCSRA] = (value, oldValue, _, _) => {
+			_cpu.Data[_config.UCSRA] = (byte)(value & (UCSRA_MPCM | UCSRA_U2X));
+			_cpu.ClearInterruptByFlag (_txc, value);
+			if ((value & UCSRA_CFG_MASK) != (oldValue & UCSRA_CFG_MASK)) {
+				OnConfigurationChange?.Invoke ();
+			}
+			return true;
+		};
+		
+		_cpu.WriteHooks[_config.UCSRB] = (value, oldValue, _, _) => {
+			_cpu.UpdateInterruptEnable (_rxc, value);
+			_cpu.UpdateInterruptEnable (_udre, value);
+			_cpu.UpdateInterruptEnable (_txc, value);
+			if ((value & UCSRB_RXEN) != 0 && (oldValue & UCSRB_RXEN) != 0) {
+				_cpu.ClearInterrupt (_rxc);
+			}
+			if ((value & UCSRB_TXEN) != 0 && (oldValue & UCSRB_TXEN) == 0) {
+				_cpu.SetInterruptFlag (_udre);
+			}
+			_cpu.Data[_config.UCSRB] = value;
+			if ((value & UCSRB_CFG_MASK) != (oldValue & UCSRB_CFG_MASK)) {
+				OnConfigurationChange?.Invoke ();
+			}
+			return true;
+		};
+		
+		_cpu.WriteHooks[_config.UCSRC] = (value, _, _, _) => {
+			_cpu.Data[_config.UCSRC] = value;
+			OnConfigurationChange?.Invoke ();
+			return true;
+		};
+		
+		_cpu.ReadHooks[_config.UDR] = _ => {
+			var mask = RxMasks.GetValueOrDefault (BitsPerChar, 0xff);
+			var result = _rxByte & mask;
+			_rxByte = 0;
+			_cpu.ClearInterrupt (_rxc);
+			return (byte)result;
+		};
+		
+		_cpu.WriteHooks[_config.UDR] = (value, _, _, _) => {
+			OnByteTransmit?.Invoke (value);
+			if (OnLineTransmit != null) {
+				var ch = (char)value;
+				if (ch == '\n') {
+					OnLineTransmit (_lineBuffer.ToString ());
+					_lineBuffer.Clear ();
+				} else {
+					_lineBuffer.Append (ch);
+				}
+			}
+			_cpu.AddClockEvent (() => {
+				_cpu.SetInterruptFlag (_udre);
+				_cpu.SetInterruptFlag (_txc);
+			}, CyclesPerChar);
+			_cpu.ClearInterrupt (_txc);
+			_cpu.ClearInterrupt (_udre);
+			return false;
+		};
+		
+		_cpu.WriteHooks[_config.UBRRH] = (value, _, _, _) => {
+			_cpu.Data[_config.UBRRH] = value;
+			OnConfigurationChange?.Invoke ();
+			return true;
+		};
+		
+		_cpu.WriteHooks[_config.UBRRL] = (value, _, _, _) => {
+			_cpu.Data[_config.UBRRL] = value;
+			OnConfigurationChange?.Invoke ();
+			return true;
+		};
+	}
+	
+	public void Reset ()
+	{
+		_cpu.Data[_config.UCSRA] = UCSRA_UDRE;
+		_cpu.Data[_config.UCSRB] = 0;
+		_cpu.Data[_config.UCSRC] = UCSRC_UCSZ1 | UCSRC_UCSZ0; // default: 8 bits per byte
+		_rxBusyValue = false;
+		_rxByte = 0;
+		_lineBuffer.Clear ();
+	}
+	
+	public bool WriteByte (byte value, bool immediate = false)
+	{
+		if (_rxBusyValue || !RxEnable) {
+			return false;
+		}
+		if (immediate) {
+			_rxByte = value;
+			_cpu.SetInterruptFlag (_rxc);
+			OnRxComplete?.Invoke ();
+		} else {
+			_rxBusyValue = true;
+			_cpu.AddClockEvent (() => {
+				_rxBusyValue = false;
+				WriteByte (value, true);
+			}, CyclesPerChar);
+			return true;
+		}
+		return false;
+	}
+}
+
+public struct UsartConfig
+{
+	public byte RxCompleteInterrupt;
+	public byte DataRegisterEmptyInterrupt;
+	public byte TxCompleteInterrupt;
+	
+	public byte UCSRA;
+	public byte UCSRB;
+	public byte UCSRC;
+	public byte UBRRL;
+	public byte UBRRH;
+	public byte UDR;
+}
